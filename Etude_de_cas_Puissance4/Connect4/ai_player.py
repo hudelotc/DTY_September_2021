94a97384ef5a49bb59aedf75f67bea862200710d
dechiktorren <dechiktorren@hotmail.fr>
2021-08-31 14:23:20 +0200

    alpha beta

--- Etude_de_cas_Puissance4/Connect4/ai_player.py
+++ Etude_de_cas_Puissance4/Connect4/ai_player.py
@@ -1,4 +1,5 @@
 from player import Player
+import copy
 
 
 class AIPlayer(Player):
@@ -11,4 +12,34 @@ class AIPlayer(Player):
     
     def getColumn(self, board):
          # TODO(student): implement this!
+        return 0
+    
+    def alphabeta(self, board, depth, alpha, beta, maximizingPlayer=True):
+        if depth == 0 or board.isFull() :
+            return self.heuristic(board)
+        if maximizingPlayer :
+            value = -1
+            for col in board.getPossibleColumns():
+                child = copy.deepcopy(board)
+                child.play(Player, col)
+                
+                value = max(value, self.alphabeta(child, depth-1, alpha, beta, False))
+                
+                if value >= beta :
+                    break # beta cutoff 
+                alpha = max(alpha, value)
+            return value
+        else:
+            value = 1000
+            for col in board.getPossibleColumns():
+                child = copy.deepcopy(board)
+                child.play(Player, col)
+                
+                value = min(value, self.alphabeta(child, depth-1, alpha, beta, True))
+                if value <= alpha :
+                    break #α cutoff 
+                beta = min(beta, value)
+            return value
+
+    def heuristic(self, board):
         return 0
